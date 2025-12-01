ae5048a6b907b6b129d33459d91a81ad8b7441cb
Meel <meel.cor@gmail.com>
2025-11-30 23:56:06 -0300

    fix

--- src/main/java/com/TpAlgoritmosFinal3/config/CorsConfig.java
+++ src/main/java/com/TpAlgoritmosFinal3/config/CorsConfig.java
@@ -8,7 +8,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://backalgoritmo.onrender.com")
+                .allowedOrigins("https://frontalgoritmo3.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedMethods("*")
                 .allowCredentials(true);
